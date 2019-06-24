9ab7042784f84c386dafdcaed8c11fd1de4b8704
Clarice Costello <claricostello@outlook.com>
2019-06-24 13:56:48 -0700

    Created APplicationUser based on IdentityUser

---
+++ ReactRummy/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace ReactRummy.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
