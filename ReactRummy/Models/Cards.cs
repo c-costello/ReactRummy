8b1ed301464645a1cd028d4de555ecfcac13ebbd
Clarice Costello <claricostello@outlook.com>
2019-06-24 13:25:24 -0700

    Created initial card model

---
+++ ReactRummy/Models/Cards.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactRummy.Models
+{
+    public class Card
+    {
+        //Composite Key
+        public SuitType Suit { get; set; }
+        public ValType Value { get; set; }
+        public LocationType Location { get; set; }
+        //Constructor
+        public Card (SuitType suit, ValType val, LocationType location)
+        {
+            Suit = suit;
+            Value = val;
+            Location = location;
+        }
+        public enum SuitType { Spades, Clubs, Hearts, Diamonds}
+        public enum ValType { Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King}
+        public enum LocationType { Start, Draw, Discard, PlayerOneHand, PlayerOneLayDown, PlayerTwoHand, PlayerTwoLayDown}
+    }
+}
