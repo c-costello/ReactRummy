026997181b10602a844af3ecb4c349ba8f01313f
Clarice Costello <claricostello@outlook.com>
2019-06-24 14:08:55 -0700

    Created GameDbContext with constructor

---
+++ ReactRummy/Data/GameDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReactRummy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactRummy.Data
+{
+    public class GameDbContext : DbContext
+    {
+        public GameDbContext(DbContextOptions<GameDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
