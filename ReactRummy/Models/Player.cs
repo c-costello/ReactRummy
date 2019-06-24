e125b7683b1718af684aa1d17e206e7386b1dbcf
Clarice Costello <claricostello@outlook.com>
2019-06-24 13:19:41 -0700

    Created initial player model

---
+++ ReactRummy/Models/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactRummy.Models
+{
+    public class Player
+    {
+        //Properties
+        public int ID { get; set; }
+        //Foreign Key from Game
+        public int GameID { get; set; }
+        public string User { get; set; } //Reference to Identity Database
+        public int Score { get; set; }
+
+
+    }
+}
