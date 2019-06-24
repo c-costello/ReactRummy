118a25fa015324f3ac51300bdc05b68bdfbabe35
Clarice Costello <claricostello@outlook.com>
2019-06-24 13:16:34 -0700

    Created initial game class

---
+++ ReactRummy/Models/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactRummy.Models
+{
+    public class Game
+    {
+        //Properties
+        public int ID { get; set; }
+        public StatusType Status { get; set; }
+        
+        public Game()
+        {
+            Status = StatusType.Open;
+        }
+
+        //Navigational Properties
+        public List<Player> Players { get; set; } 
+
+        //Enums
+        public enum StatusType { Open, InGame, Over}
+
+    }
+}
