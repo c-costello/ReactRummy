088eb9978122edd64d93e0bbb4685a5fc9e72536
Clarice Costello <claricostello@outlook.com>
2019-06-25 10:00:01 -0700

    Wrote tests for Game getters and setters

---
+++ ReactRummyTests/GetterSetter/GameGetSet.cs
@@ -0,0 +1,50 @@
+using System;
+using Xunit;
+using ReactRummy.Models;
+
+namespace ReactRummyTests
+{
+    public class UnitTest1
+    {
+        public Game game = new Game()
+        {
+            ID = 1,
+            Status = Game.StatusType.Open,
+            Winner = 1,
+        };
+
+        [Fact]
+        public void CanGetID()
+        {
+            Assert.Equal(1, game.ID);
+        }
+        [Fact]
+        public void CanGetStatus()
+        {
+            Assert.Equal(Game.StatusType.Open, game.Status);
+        }
+        [Fact]
+        public void CanGetWinner()
+        {
+            Assert.Equal(1, game.Winner);
+        }
+        [Fact]
+        public void CanSetID()
+        {
+            game.ID = 2;
+            Assert.Equal(2, game.ID);
+        }
+        [Fact]
+        public void CanSetStatus()
+        {
+            game.Status = Game.StatusType.Over;
+            Assert.Equal(Game.StatusType.Over, game.Status);
+        }
+        [Fact]
+        public void CanSetWinner()
+        {
+            game.Winner = 2;
+            Assert.Equal(2, game.Winner);
+        }
+    }
+}
