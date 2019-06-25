f652fb79dd611bf7cbfab919ad4ed23df35c90ce
Clarice Costello <claricostello@outlook.com>
2019-06-25 09:44:29 -0700

    Created Location Interface

---
+++ ReactRummy/Models/Interfaces/ILocation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactRummy.Models.Interfaces
+{
+    public interface ILocation 
+    {
+        Task<Location> GetLocation(int id);
+        Task<IEnumerable<Location>> GetLocations();
+        Task CreateLocation(Location location);
+        Task DeleteLocation(int id);
+        Task UpdateLocation(Location location);
+    }
+}
