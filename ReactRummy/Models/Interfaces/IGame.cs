45ccebaff0dd7aea20c912abb9f0783af49c2308
Clarice Costello <claricostello@outlook.com>
2019-06-25 09:10:41 -0700

    Created Game interface

---
+++ ReactRummy/Models/Interfaces/IGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactRummy.Models.Interfaces
+{
+    public interface IGame 
+    {
+        Task<IEnumerable<Game>> GetGames();
+        Task<Game> GetGame(int id);
+        Task<Game> CreateGame(Game game);
+        Task<Game> UpdateGame(Game game);
+        Task<Game> DeleteGame(int id);
+    }
+}
