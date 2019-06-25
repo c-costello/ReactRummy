6bc7abffa664b944a7290a75ff150f8d2cd25752
Clarice Costello <claricostello@outlook.com>
2019-06-25 09:32:17 -0700

    Created Card interface

---
+++ ReactRummy/Models/Interfaces/ICard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactRummy.Models.Interfaces
+{
+    public interface ICard
+    {
+        Task<Card> GetCard(Card.SuitType suit, Card.ValType val);
+        Task<IEnumerable<Card>> GetCards();
+        Task<IEnumerable<Card>> GetCardsByLocation(int locationID);
+        Task CreateCard(Card card);
+        Task UpdateCard(Card card);
+        Task DeleteCard(Card.SuitType suit, Card.ValType val);
+    }
+}
