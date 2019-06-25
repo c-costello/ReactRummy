b3a046b87efebdf81eb8182751dcfb042203f0b8
Clarice Costello <claricostello@outlook.com>
2019-06-25 09:20:57 -0700

    Created player interface

---
+++ ReactRummy/Models/Interfaces/IPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactRummy.Models.Interfaces
+{
+    public interface IPlayer
+    {
+        Task<Player> GetPlayer(int id);
+        Task<IEnumerable<Player>> GetPlayersByGame(int gameID);
+        Task<IEnumerable<Player>> GetPlayers();
+        Task<Player> CreatePlayer(Player player);
+        Task<Player> UpdatePlayer(Player player);
+        Task DeletePlayer(int id);
+
+
+    }
+}
